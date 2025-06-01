e0e6ed4cca6e16c68298bcc791daaa845fbeb92a
22b1074 <130027346+22b1074@users.noreply.github.com>
2025-06-01 12:47:37 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://vectorshift-assessment-frontend.onrender.com/",
+    "https://vectorshift-assessment-frontend.onrender.com",
     # add other origins if needed
 ]
 
